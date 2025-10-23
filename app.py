a6eb51d22a84af207610cbfb43484ad61d969921
Henrico Birochi <henrico.birochi@gmail.com>
2025-10-23 09:54:10 -0300

    feat: Ensure that the database is created when starting the app

--- app.py
+++ app.py
@@ -12,5 +12,13 @@ db.init_app(app)
 # Registro do Blueprint de tarefas
 app.register_blueprint(task_bp)
 
+def ensure_db(app):
+    """Create database tables if they don't exist."""
+    with app.app_context():
+        db.create_all()
+
+
 if __name__ == '__main__':
+    # Ensure tables exist before starting the dev server
+    ensure_db(app)
     app.run(debug=True)
