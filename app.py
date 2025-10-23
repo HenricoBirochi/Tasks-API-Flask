75f05fdc560f6b625b87ad7194d1df60113c9a7a
Henrico Birochi <henrico.birochi@gmail.com>
2025-10-22 22:28:29 -0300

    config: app.py configured

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from extensions import db
+from controller.task_controller import task_bp
 
+# Configuração do app Flask
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, World!"
+# Configuração do banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db.init_app(app)
+
+# Registro do Blueprint de tarefas
+app.register_blueprint(task_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
